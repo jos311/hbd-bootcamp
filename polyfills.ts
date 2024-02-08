5acd360d7e45dc9ffdbf1e3aaca9067512ac712a
jos311 <jose_ramirezmoran@student.uml.edu>
2024-02-08 18:04:58 -0500

    commit2

--- polyfills.ts
+++
@@ -1,5 +0,0 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { Buffer } from "buffer";
-(window as any).global = window;
-(window as any).global.Buffer = Buffer;
-(window as any).process = {env: { }};
